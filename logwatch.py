42d5c52e54c1dc4c555a67fd2893f629d5b21c20
Kae Sackett <ksackett@cli-16195.office.atlassian.com>
2017-05-31 15:33:00 -0500

    Added alerting thresholds.

--- logwatch.py
+++ logwatch.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-"""A simple console program that monitors HTTP traffic
-on a machine and provides some summative metrics."""
+"""
+A simple console program that monitors HTTP traffic
+on a machine and provides some summative metrics.
+
+Example Usage: python logwatch.py -f access.log -h 100
+"""
 
 import threading
 import operator
-import os.path
 import tailer # Available in PIP
 import time
+import os
 
 from optparse import OptionParser
 
@@ -15,6 +19,8 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-f", "--filename", dest="filename",
                   help="The filename of the log you'd like to monitor.")
+parser.add_option("-m", "--max", type=int, dest="high_threshold",
+                  help="The critical threshold at which to alert for high traffic volume.")
 options, args = parser.parse_args()
 
 class LogWatcher(object):
@@ -30,15 +36,19 @@ class LogWatcher(object):
     self.stats["protocol"] = {}
     self.stats["response"] = {}
     self.total = 0
+    self.queue = []
+    self.alert = False
 
   def watch_file(self):
     """Follow a logfile indefinitely."""
 
     for line in tailer.follow(open(self.filename)):
-      print("I GOT HERE")
-      lw.process_line(line)
+      self.process_line(line)
+      self.queue.append(time.time())
+      while time.time() - self.queue[0] > 119:
+        self.queue.pop(0)
 
-  def process_line(line):
+  def process_line(self, line):
     """Parse w3c-formatted log lines."""
 
     first, rest = line.split(']')
@@ -58,23 +68,31 @@ class LogWatcher(object):
     print("Usage stats:")
 
     # Total Requests
-    print(self.total)
+    print("Total Requests: {}".format(self.total))
 
     # Top IP and percent of total requests
     top_ip = sorted(self.stats["ip"].items(), key=operator.itemgetter(1), reverse=True)
-    print("Top IP: {0} ({1}%)".format(top_ip[0], (self.stats["ip"][top_ip] / self.total * 100)))
+    print("Top IP: {0} ({1}%)".format(top_ip[0][0], (top_ip[0][1] / self.total * 100)))
 
     # Most common request method
     most_common_request = sorted(self.stats["method"].items(), key=operator.itemgetter(1), reverse=True)
-    print("Most Common Request: {}".format(most_common_request[0]))
+    print("Most Common Request: {}".format(most_common_request[0][0], (most_common_request[0][1] / self.total * 100)))
 
     # Raw number of GETs and POSTs
-    print("GETs: {0} ({1}%)".format(self.stats["method"]["GET"]), (self.stats["method"]["GET"] / self.total * 100))
-    print("POSTs: {0} ({1}%)".format(self.stats["method"]["POST"]), (self.stats["method"]["POST"] / self.total * 100))
+    print("GETs: {0} ({1}%)".format(self.stats["method"].get("GET", 0), self.stats["method"].get("GET", 0) / self.total * 100))
+    print("POSTs: {0} ({1}%)".format(self.stats["method"].get("POST", 0), self.stats["method"].get("POST", 0) / self.total * 100))
 
     # Most frequently requested section
     most_frequent_section = sorted(self.stats["section"].items(), key=operator.itemgetter(1), reverse=True)
-    print("Most Frequently Requested Section: {0} ({1}%)".format(most_frequent_section[0], (self.stats["section"][most_frequent_section] / self.total * 100)))
+    print("Most Frequently Requested URI: {0} ({1}%)".format(most_frequent_section[0][0], (most_frequent_section[0][1] / self.total * 100)))
+
+    if len(self.queue) > options.high_threshold:
+      print("High traffic generated an alert - hits = {0}, triggered at{1}".format(len(self.queue), time.strftime('%l:%M%p %z on %b %d')))
+      self.alert = True
+    else:
+      if self.alert:
+        self.alert = False
+        print("High traffic alert resolved at {}.".format(time.strftime('%l:%M%p %z on %b %d')))
 
 def main():
   # Handle FNF exceptions
@@ -89,9 +107,10 @@ def main():
   thread = threading.Thread(target=lw.watch_file)
   thread.start()
 
-  # Console output every 20 seconds
+  # Console output every 10 seconds
   while True:
     time.sleep(10)
+    os.system('clear')
     lw.dump()
 
 if __name__ == "__main__":
